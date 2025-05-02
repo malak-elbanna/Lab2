2a62d2491920fd1f2cda04962c54301537dd34e9
malakelbanna <malakelbanna2004@gmail.com>
2025-05-02 20:50:16 +0300

    updated server link

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const auth = require('./routes/auth');
-const user = require('./routes/user');
 const dotenv = require('dotenv').config();
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('mongo connected'))
 
-    const logger = (req, res, next) => {
+const logger = (req, res, next) => {
     console.log(`[${Date.now()}] ${req.method} ${req.originalUrl}`);
     next();
 };
 
 app.use(logger);
 app.use(express.json());
-
-app.use("/api/auth", auth);
-app.use("/api/user", user); 
 
 app.get("/", (req, res) => {
     res.send("heyyyy");
